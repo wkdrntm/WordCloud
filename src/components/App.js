aa82c7c0cdbb320b82012c786208345e3da88339
wkdrntm <wkdrmtm@naver.com>
2019-06-26 16:16:06 +0900

    Add Navigation Bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import AppShell from './AppShell'
 
 class App extends React.Component{
     render(){
         return (
-        <div>
-            <h3>hello world</h3>
-        </div>
+        // <div>
+        //     <h3>hello world</h3>
+        // </div>
+            <AppShell/>
         );
     }
 }
